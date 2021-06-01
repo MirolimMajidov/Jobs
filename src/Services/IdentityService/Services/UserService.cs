f36e8a12f395140908b73e8a903417160d0a03de
Mirolim Majidov <majidovmirolimjon@gmail.com>
2021-06-01 10:45:57 +0500

    Implemented gRPC for IdentityService to get user info

--- src/Services/IdentityService/Services/UserService.cs
+++ src/Services/IdentityService/Services/UserService.cs
@@ -1,28 +1,62 @@
 using Grpc.Core;
 using IdentityService.Protos;
 using Microsoft.Extensions.Logging;
+using Service.SharedModel.Repository;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using static IdentityService.Protos.User;
 
 namespace IdentityService.Services
 {
-    public class UserService : TestUser.TestUserBase
+    public class UserService : UserBase
     {
-        private readonly ILogger<UserService> _logger;
-        public UserService(ILogger<UserService> logger)
+        private readonly IEntityRepository<Models.User> _pepository;
+        private readonly ILogger _logger;
+
+        public UserService(ILogger<UserService> logger, IEntityRepository<Models.User> pepository)
         {
             _logger = logger;
+            _pepository = pepository;
         }
 
-        public override Task<UserReply> GetById(UserRequest request, ServerCallContext context)
+        public override async Task<UserReply> GetUserById(UserRequest request, ServerCallContext context)
         {
-            return Task.FromResult(new UserReply
+            _logger.LogInformation("Begin grpc call UserService.GetUserById for user id {Id}", request.Id);
+
+            if (Guid.TryParse(request.Id, out Guid userId))
             {
-                Id = request.Id,
-                Name = "Test"
-            });
+                var user = await _pepository.GetEntityByID(userId);
+                if (user == null)
+                {
+                    context.Status = new Status(StatusCode.NotFound, $"User with id {userId} do not exist");
+                    return null;
+                }
+
+                return await Task.FromResult(ConvertToGRPCUser(user));
+            }
+            else
+            {
+                context.Status = new Status(StatusCode.InvalidArgument, $"Id must be Guid type (received {request.Id})");
+                return null;
+            }
+        }
+
+        public override async Task GetUsers(UsersRequest request, IServerStreamWriter<UserReply> responseStream, ServerCallContext context)
+        {
+            _logger.LogInformation("Begin grpc call UserService.GetUsers");
+
+            var users = await _pepository.GetEntities();
+            foreach (var user in users)
+                await responseStream.WriteAsync(ConvertToGRPCUser(user));
+        }
+
+        static UserReply ConvertToGRPCUser(Models.User user)
+        {
+            return new UserReply
+            {
+                Id = user.Id.ToString(),
+                Name = user.Name
+            };
         }
     }
 }
