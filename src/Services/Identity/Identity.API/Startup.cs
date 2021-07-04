4f57449bc6ee862896d2077557871434c85da8d7
Mirolim Majidov <majidovmirolimjon@gmail.com>
2021-07-04 16:02:49 +0500

    fixed tests

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -42,7 +42,9 @@ namespace IdentityService
             sqlOptions => sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(10), errorNumbersToAdd: null)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
             services.AddTransient(typeof(IEntityRepository<>), typeof(EntityRepository<>));
-            services.UseEventBusRabbitMQ(Configuration["RabbitMQHostName"], Configuration["SubscriptionClientName"], int.Parse(Configuration["EventBusRetryCount"]));
+
+            int eventBusRetryCount = Configuration.GetSection("Environment").Value == "Test" ? 0 : int.Parse(Configuration["EventBusRetryCount"]);
+            services.UseEventBusRabbitMQ(Configuration["RabbitMQHostName"], Configuration["SubscriptionClientName"], eventBusRetryCount);
 
             services.AddAuthenticationsAndPolices();
             services.AddControllers(options => options.Filters.Add(typeof(JobsExceptionFilter)))
@@ -70,8 +72,7 @@ namespace IdentityService
             app.UseRouting();
             app.UseAuthorization();
 
-            if (!env.IsEnvironment("Test"))
-                app.UseEventBus();
+            app.UseEventBus();
 
             app.UseEndpoints(endpoints =>
             {
