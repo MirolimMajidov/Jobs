cab50d0e0052c25e6c4183fafb9629713fa2fafd
Mirolim Majidov <Mirolim@silkroadprofessionals.com>
2021-07-07 15:03:56 +0500

    Updated APIs docs and refactored TestGrpcClient project

--- src/Services/Identity/Tests/IdentityAPIGrpcClient/Program.cs
+++ src/Services/Identity/Tests/IdentityAPIGrpcClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Grpc.Net.Client;
 using IdentityClient.Protos;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace TestGrpcClient
@@ -9,21 +11,38 @@ namespace TestGrpcClient
     {
         static async Task Main(string[] args)
         {
-            var grpcChannel = GrpcChannel.ForAddress("http://localhost:5101/");
+            var grpcChannel = GrpcChannel.ForAddress("http://localhost:7101/");
             var client = new User.UserClient(grpcChannel);
-            using (var clientData = client.GetUsers(new UsersRequest()))
-            {
-                while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
-                {
-                   var user = clientData.ResponseStream.Current;
-                    Console.WriteLine(user);
-                }
-            }
-            Console.ReadLine();
 
-            var userResponce = await client.GetUserByIdAsync(new UserRequest { Id = "1987cf71-5519-440b-9cb9-7f62cbadfba8" });
-            Console.WriteLine(userResponce);
-            Console.ReadLine();
+            //Get all users
+            Console.WriteLine("All users: ");
+            var users = await GetUsers(client);
+            foreach (var user in users)
+                Console.WriteLine(user);
+
+            Console.WriteLine();
+
+            //Get user info by ID
+            Console.WriteLine("User info: ");
+            var userInfo = await GetUserById(client, users.FirstOrDefault()?.Id);
+            Console.WriteLine(userInfo);
+        }
+
+        static async Task<List<UserReply>> GetUsers(User.UserClient client)
+        {
+            List<UserReply> users = new();
+            using var clientData = client.GetUsers(new UsersRequest());
+            while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+            {
+                var user = clientData.ResponseStream.Current;
+                users.Add(user);
+            }
+            return users;
+        }
+
+        static async Task<UserReply> GetUserById(User.UserClient client, string userId)
+        {
+            return await client.GetUserByIdAsync(new UserRequest { Id = userId });
         }
     }
 }
