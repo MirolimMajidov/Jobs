485e3d48e70f75e3af77e26fb171ccc2838c913a
Mirolim Majidov <mirolimmajidov@outlook.com>
2024-06-26 14:35:13 +0500

    Fixed integration tests

--- src/Services/Identity/Tests/Identity.FunctionalTests/BaseTestEntity.cs
+++ src/Services/Identity/Tests/Identity.FunctionalTests/BaseTestEntity.cs
@@ -1,41 +1,22 @@
-﻿using Jobs.Service.Common;
-using Microsoft.AspNetCore.Hosting;
+﻿using Identity.FunctionalTests;
+using Jobs.Service.Common;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
-using System.IO;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace IdentityService.FunctionalTests
 {
     public abstract class BaseTestEntity
     {
-        protected TestServer Server; 
+        protected TestServer Server;
         protected HttpClient Client;
-
-        private TestServer CreateTestServer()
-        {
-            var path = Assembly.GetAssembly(typeof(Startup)).Location;
-
-            var hostBuilder = new WebHostBuilder()
-                .UseEnvironment("Test")
-                .UseContentRoot(Path.GetDirectoryName(path))
-                .ConfigureAppConfiguration(contextBuilder =>
-                {
-                    contextBuilder.AddJsonFile("appsettings.json", optional: false).AddEnvironmentVariables();
-                })
-                .UseStartup<Startup>();
-
-            return new TestServer(hostBuilder);
-        }
 
         [TestInitialize]
         public void SetUp()
         {
-            Server = CreateTestServer();
+            Server = new ServerApiFactory().Server;
             Client = Server.CreateClient();
         }
 
@@ -46,11 +27,6 @@ namespace IdentityService.FunctionalTests
             Client?.Dispose();
             Server = null;
             Client = null;
-        }
-
-        public string GenaretJson(object requestModel)
-        {
-            return RequestModel.GenaretJson(requestModel);
         }
     }
 
---
+++ src/Services/Identity/Tests/Identity.FunctionalTests/ServerApiFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Identity.FunctionalTests
+{
+    using IdentityService;
+    using IdentityService.DataProvider;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Linq;
+
+    public class ServerApiFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test");
+
+            builder.ConfigureServices(services =>
+            {
+                //// Remove the existing context configuration
+                //var descriptor = services.SingleOrDefault(
+                //    d => d.ServiceType == typeof(DbContextOptions<JobsContext>));
+                //if (descriptor != null)
+                //    services.Remove(descriptor);
+
+                //// Add an in-memory database for testing
+                //services.AddDbContext<JobsContext>(options =>
+                //{
+                //    options.UseInMemoryDatabase("JobDB");
+                //});
+            });
+        }
+    }
+}
