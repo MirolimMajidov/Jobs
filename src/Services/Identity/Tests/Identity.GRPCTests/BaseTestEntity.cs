d2c449e6d5f2451ad8b76ca37e4d985363157a29
Mirolim Majidov <mirolimmajidov@outlook.com>
2024-06-26 16:06:38 +0500

    Added tests for gRPC

---
+++ src/Services/Identity/Tests/Identity.GRPCTests/BaseTestEntity.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Identity.GRPCTests
+{
+    public abstract class BaseTestEntity
+    {
+        protected TestServer Server;
+        protected GrpcChannel _grpcChannel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Server = new ServerApiFactory().Server;
+
+            // Create a gRPC channel
+            _grpcChannel = GrpcChannel.ForAddress(Server.BaseAddress, new GrpcChannelOptions
+            {
+                HttpClient = Server.CreateClient()
+            });
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Server?.Dispose();
+            _grpcChannel?.Dispose();
+            Server = null;
+            _grpcChannel = null;
+        }
+    }
+}
