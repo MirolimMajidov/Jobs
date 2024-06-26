bff197a035fa1cb34d3c01682136ed94c6882d18
Mirolim Majidov <mirolimmajidov@outlook.com>
2024-06-26 14:58:14 +0500

    Refactored running integration tests

--- src/Services/Identity/Tests/Identity.FunctionalTests/ServerApiFactory.cs
+++ src/Services/Identity/Tests/Identity.FunctionalTests/ServerApiFactory.cs
@@ -7,6 +7,9 @@ namespace Identity.FunctionalTests
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+    using System;
     using System.Linq;
 
     public class ServerApiFactory : WebApplicationFactory<Program>
@@ -17,18 +20,41 @@ namespace Identity.FunctionalTests
 
             builder.ConfigureServices(services =>
             {
-                //// Remove the existing context configuration
-                //var descriptor = services.SingleOrDefault(
-                //    d => d.ServiceType == typeof(DbContextOptions<JobsContext>));
-                //if (descriptor != null)
-                //    services.Remove(descriptor);
+                // Remove the existing context configuration
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<JobsContext>));
+                if (descriptor != null)
+                    services.Remove(descriptor);
 
-                //// Add an in-memory database for testing
-                //services.AddDbContext<JobsContext>(options =>
-                //{
-                //    options.UseInMemoryDatabase("JobDB");
-                //});
+                // Add an in-memory database for testing
+                services.AddDbContext<JobsContext>(options =>
+                {
+                    options.UseInMemoryDatabase("JobDB");
+                });
             });
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var host = base.CreateHost(builder);
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                var dbContext = services.GetRequiredService<JobsContext>();
+
+                dbContext.SeedAsync(logger).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while seeding the database.");
+
+                throw;
+            }
+
+            return host;
         }
     }
 }
